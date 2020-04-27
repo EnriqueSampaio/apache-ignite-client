f17baa6d0c23e3f396c237df3600b32100c06c6c
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2020-04-27 18:33:04 -0300

    Adding types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,12 @@
-export declare class Timestamp extends Date {
+export class Timestamp extends Date {
   constructor(time: number, nano: number);
   getNanos(): number;
   setNanos(): this;
+}
+export namespace IgniteClient {
+  enum STATE {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED
+  }
 }
